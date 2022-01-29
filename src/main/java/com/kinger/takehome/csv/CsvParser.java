cd3dba9539979dac198c46b8b7cc6e7bfc58b4f1
Larry King <kingerredsox@gmail.com>
2022-01-29 17:26:40 -0500

    Code and test for headers with empty columns or duplicate columns.  Those are throwing an exception since it would be impossible to sort on blank or a duplicate.

--- src/main/java/com/kinger/takehome/csv/CsvParser.java
+++ src/main/java/com/kinger/takehome/csv/CsvParser.java
@@ -1,38 +1,29 @@
 package com.kinger.takehome.csv;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 import java.nio.file.Files;
-import java.util.HashMap;
-import java.util.Map;
+import java.security.InvalidParameterException;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CsvParser {
-    Stream<String> csvFileStream;
+    private final Stream<String> csvFileStream;
     private final ColumnHeader headers;
 
-    private static final class ColumnHeader {
-        Map<String, Integer> header;
-        String headerLine;
+    // Using a record removes boilerplate constructor and field initialization
+    private record ColumnHeader(Map<String, Integer> header, String headerLine) {
+    }
 
-        ColumnHeader(String headerLine) {
-            this.headerLine = headerLine;
-            createHeaderMap(headerLine);
-        }
-
-        private void createHeaderMap(String headerLine) {
-            System.out.println(headerLine);
-            header = new HashMap<>();
-        }
-
+    public static CsvParser parse(final File file) throws IOException {
+        return new CsvParser(file);
     }
 
     public CsvParser(File csvFile) throws IOException {
-        csvFileStream = Files.lines(csvFile.toPath());
-        headers = new ColumnHeader(csvFileStream.findFirst().get());
-        // csvFileStream.forEach(System.out::println);
+        this.csvFileStream = Files.lines(csvFile.toPath());
+        this.headers = createHeader(Files.lines(csvFile.toPath()).findFirst().get());
+        Files.lines(csvFile.toPath()).forEach(System.out::println);
     }
 
     public String getHeader() {
@@ -43,8 +34,37 @@ public class CsvParser {
         return this.headers.header;
     }
 
-    public static CsvParser parse(final File file) throws IOException {
-        return new CsvParser(file);
+    public List<String> parseLine(String csvDelimitedString) {
+        return Stream.of(csvDelimitedString.split(",", -1)).collect(Collectors.toList());
+    }
+
+    public boolean hasEmptyElements(List<String> tokens) {
+        for (String s : tokens) {
+            if (s.length() == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasDuplicateElements(List<String> tokens) {
+        Set<String> duplicates = new HashSet<>();
+        for(String s : tokens) {
+            if(!duplicates.add(s)) return true;
+        }
+        return false;
+    }
+
+    private ColumnHeader createHeader(String headerLine) {
+        List<String> headerList = parseLine(headerLine);
+        if(hasEmptyElements(headerList)) throw new InvalidParameterException("Header row cannot have empty columns");
+        if(hasDuplicateElements(headerList)) throw new InvalidParameterException("Header row cannot have duplicate columns");
+
+        Map<String, Integer> header = new HashMap<>();
+        for (int i = 0; i < headerList.size(); i++) {
+            header.put(headerList.get(i), i);
+        }
+        return new ColumnHeader(header, headerLine);
     }
 
 }
--- src/test/java/com/kinger/takehome/csv/CsvParserTest.java
+++ src/test/java/com/kinger/takehome/csv/CsvParserTest.java
@@ -1,31 +1,44 @@
 package com.kinger.takehome.csv;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.InvalidParameterException;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CsvParserTest {
-    String headerString = "TeamName,City,State,Division";
-    CsvParser sut;
-
-    @BeforeEach
-    void setUp() throws IOException {
-        File testFile = new File(Objects.requireNonNull(getClass().getClassLoader().getResource("test.csv")).getFile());
-        sut = CsvParser.parse(testFile);
-    }
+    final String columnHeaders = "TeamName,City,State,Division";
+    int numColumns = 4;
 
     @Test
     void HeaderShouldMatch() {
-        assertEquals(headerString, sut.getHeader());
+        CsvParser sut = null;
+        File testFile = new File(Objects.requireNonNull(getClass().getClassLoader().getResource("mlbtest.csv")).getFile());
+        try {
+            sut = CsvParser.parse(testFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertEquals(columnHeaders, sut.getHeader());
+        assertEquals(numColumns, sut.getHeaderMap().size());
     }
 
     @Test
-    void HeaderShouldHave3Columns() {
-        assertEquals(4, sut.getHeaderMap().size());
+    void EmptyHeaderFieldShouldThrowException() {
+        CsvParser sut = null;
+        File testFile = new File(Objects.requireNonNull(getClass().getClassLoader().getResource("emptyHeaderFieldTest.csv")).getFile());
+        InvalidParameterException ipe = assertThrows(InvalidParameterException.class, () -> CsvParser.parse(testFile));
+        assertEquals("Header row cannot have empty columns", ipe.getMessage());
+    }
+
+    @Test
+    void DuplicateHeaderFieldShouldThrowException() {
+        CsvParser sut = null;
+        File testFile = new File(Objects.requireNonNull(getClass().getClassLoader().getResource("duplicateHeaderFieldTest.csv")).getFile());
+        InvalidParameterException ipe = assertThrows(InvalidParameterException.class, () ->  CsvParser.parse(testFile));
+        assertEquals("Header row cannot have duplicate columns", ipe.getMessage());
     }
 }
